a2cef167b50769a37d5734197f1fcee1001de3ab
taphuk4 <114635264+taphuk4@users.noreply.github.com>
2025-01-06 08:31:49 +0000

    update

--- hakai/lib/screens/home_screen.dart
+++ hakai/lib/screens/home_screen.dart
@@ -1,118 +1,169 @@
 import 'package:flutter/material.dart';
-import 'package:hakai/screens/expansion_tile_card.dart'; 
 
-class HomePage extends StatefulWidget{
-  
+class User {
+  String username;
+  String password;
+  String role;
+
+  User(this.username, this.password, this.role);
+}
+
+class HomePage extends StatefulWidget {
   @override
-  
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
-  
+
+  // Danh sách người dùng
+  final List<User> users = [
+    User('Phu', '1', 'admin1'),
+    User('Long', '2', 'editor1'),
+    User('Minh', '3', 'viewer1'),
+    User('Thanh', '4', 'admin1'),
+    User('Kong', '5', 'edito1r'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         backgroundColor: Colors.orange,
-        title: Text('hello kitty'),
-        
+        title: Text('Hello Kitty'),
       ),
-       
       drawer: Drawer(
-  child: ListView(
-    padding: EdgeInsets.zero,
-    children: [
-      UserAccountsDrawerHeader(
-        decoration: const BoxDecoration(
-          color: Colors.green,
-        ),
-        accountName: const Text(
-          "Đỗ Đăng Minh",
-          style: TextStyle(fontSize: 18),
-        ),
-        accountEmail: const Text("daikaminhz178@gmail.com"),
-        currentAccountPictureSize: const Size.square(50),
-        currentAccountPicture: const CircleAvatar(
-          backgroundColor: Color.fromARGB(255, 165, 255, 137),
-          child: Text(
-            "A",
-            style: TextStyle(fontSize: 30.0, color: Colors.blue),
-          ),
-        ),
-      ),
-      ListTile(
-        leading: const Icon(Icons.person),
-        title: const Text('My Profile'),
-        onTap: () {
-          Navigator.pop(context);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.book),
-        title: const Text('My Course'),
-        onTap: () {
-          Navigator.pop(context);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.workspace_premium),
-        title: const Text('Go Premium'),
-        onTap: () {
-          Navigator.pop(context);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.video_label),
-        title: const Text('Saved Videos'),
-        onTap: () {
-          Navigator.pop(context);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.edit),
-        title: const Text('Edit Profile'),
-        onTap: () {
-          Navigator.pop(context);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.logout),
-        title: const Text('Log Out'),
-        onTap: () {        
-         Navigator.of(context).pushReplacementNamed('/');
-        },
-      ),
-    ],
-  ),
-),
-
-         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,   
-
-          fixedColor: Colors.green,
-          
-          items: const [
-            BottomNavigationBarItem(
-              label: "Home",
-              icon: Icon(Icons.home),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.green,
+              ),
+              accountName: const Text(
+                "Đỗ Đăng Minh",
+                style: TextStyle(fontSize: 18),
+              ),
+              accountEmail: const Text("taphuk4@gmail.com"),
+              currentAccountPictureSize: const Size.square(50),
+              currentAccountPicture: const CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 165, 255, 137),
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 30.0, color: Colors.blue),
+                ),
+              ),
             ),
-            BottomNavigationBarItem(
-              label: "Search",
-              icon: Icon(Icons.search),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('My Profile'),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
-            BottomNavigationBarItem(
-              label: "Profile",
-              icon: Icon(Icons.account_circle),
+            ListTile(
+              leading: const Icon(Icons.book),
+              title: const Text('My Course'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.workspace_premium),
+              title: const Text('Go Premium'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.video_label),
+              title: const Text('Saved Videos'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text('Edit Profile'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Log Out'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
             ),
           ],
-          onTap: (int indexOfItem) {}),
+        ),
+      ),
+      body: _currentIndex == 0
+          ? Padding(
+              padding: const EdgeInsets.all(10),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // Hiển thị 2 cột
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 3 / 2,
+                ),
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          users[index].username,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text('Role: ${users[index].role}'),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          : Center(
+              child: Text(
+                _currentIndex == 1 ? 'Search Page' : 'Profile Page',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        fixedColor: Colors.green,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+        onTap: (int indexOfItem) {
+          setState(() {
+            _currentIndex = indexOfItem;
+          });
+        },
+      ),
     );
-    
   }
 }
-class MyAppState extends ChangeNotifier {
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
